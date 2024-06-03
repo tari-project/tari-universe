e7c8e7d9ef0dd5d000fa3707d393a1aeb0df0553
Maciej Kozuszek <mackozuszek@gmail.com>
2024-06-03 14:32:58 +0200

    Fix if there are no system folders

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -100,8 +100,18 @@ pub fn run() {
       ]
     )
     .setup(|app| {
-      let data_dir_path = app.path().app_data_dir().unwrap().to_path_buf();
-      let log_path = app.path().app_log_dir().unwrap().to_path_buf();
+      let data_dir_path = app.path().app_data_dir().unwrap();
+      if !data_dir_path.exists() {
+        std::fs::create_dir(&data_dir_path).unwrap();
+      }
+      let data_dir_path = data_dir_path.to_path_buf();
+
+      let log_path = app.path().app_log_dir().unwrap();
+      if !log_path.exists() {
+        std::fs::create_dir(&log_path).unwrap();
+      }
+      let log_path = log_path.to_path_buf();
+
       tauri::async_runtime::spawn(async move {
         start_wallet_daemon(log_path, data_dir_path).await.unwrap(); // TODO handle error while starting wallet daemon https://github.com/orgs/tari-project/projects/18/views/1?pane=issue&itemId=63753279
       });
