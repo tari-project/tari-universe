60a3e513b46a47f520d07f137beff20be20f38bd
karczuRF <oskar.karcz@rumblefish.pl>
2024-11-27 12:00:57 +0100

    feat: display nr of tapp providers

--- src/components/Account.tsx
+++ src/components/Account.tsx
@@ -4,16 +4,23 @@ import { useTranslation } from "react-i18next"
 import { accountSelector } from "../store/account/account.selector"
 import { substateIdToString } from "@tari-project/typescript-bindings"
 import { shortenSubstateAddress } from "../helpers/address"
+import { tappletProviderSelector } from "../store/tappletProviders/tappletProviders.selector"
 
+// TODO this component is just tmp to show and control provider/account
 export const Account: React.FC = () => {
   const { t } = useTranslation(["components", "common"])
   const currentAccount = useSelector(accountSelector.selectAccount)
-
+  const tappProviders = useSelector(tappletProviderSelector.getAllTappletProviders)
   const accountAddress = substateIdToString(currentAccount?.account.address ?? null)
+
   return (
     <>
       <Paper variant="outlined" elevation={0} sx={{ padding: 1, borderRadius: 2, width: "100%" }}>
         <Stack direction="column" justifyContent="flex-end">
+          <Typography
+            variant="caption"
+            textAlign="left"
+          >{`Nr of Tapplet Providers: ${tappProviders.ids.length} `}</Typography>
           <Typography
             variant="caption"
             textAlign="left"
